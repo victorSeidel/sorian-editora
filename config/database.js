2eb308ddc6641ec7c991c661b865e5f9145e3c95
Victor Seidel <victorgabrielms88@gmail.com>
2025-06-21 14:43:24 -0300

    Add port em database.js

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
     pool: 
